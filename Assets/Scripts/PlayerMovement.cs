ddcab1ac13739b57ebb660c2a670cae79b84bd57
SsilverGale <147530953+SsilverGale@users.noreply.github.com>
2024-01-28 14:45:45 -0500

    Collision
    you collide now

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -21,5 +22,10 @@ public class PlayerMovement : MonoBehaviour
             body.AddForce(-moveSpeed * transform.right);
             Debug.Log("left");
         }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene("Honk Honk");
+        Debug.Log("colliding");
     }
 }
